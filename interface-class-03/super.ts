fda27d8d67af1714c18fddd9640ba29457283ecb
+W <willohyayo@gmail.com>
2021-02-17 08:43:43 +0800

    static and super

---
+++ interface-class-03/super.ts
@@ -0,0 +1,24 @@
+class TestSuperClass {
+    constructor(
+        public p1: number,
+        public p2: string,
+        public p3: boolean,
+    ) {}
+}
+
+class TestChildClass1 extends TestSuperClass {
+    constructor(
+        p1Child: number,
+        p2Child: string,
+        p3Child: boolean,
+    ) {
+        super(p1Child, p2Child, p3Child);
+    }
+}
+
+const objFromChildClass1 = new TestChildClass1(123, 'Hello', true);
+console.log(objFromChildClass1);
+
+class TestChildClass2 extends TestSuperClass { }
+
+const objFromChildClass2 = new TestChildClass2();
